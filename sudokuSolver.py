e3859ec1abde431359e66eb85c68414315e70c83
sauertopf <freytag64@gmail.com>
2022-11-14 18:15:09 +0100

    increase read-in speed of sudokus from polynomial to linear

--- sudokuSolver.py
+++ sudokuSolver.py
@@ -4,43 +4,98 @@ from sudokuGeneralKNF import *
 import sudokus
 from sudokus import *
 
+import time
 
-def unitResolution(knf : list, unitClause : tuple) -> list:
-    """makes unitresolution to given knf for new unitclause and with that newly created ones
+
+# a smaller knf, which can be used to construct the full knf for the solver
+__generalKNF__ = [
+    [(False, 'h'), (False, 'i')],
+    [(False, 'g'), (False, 'i')],
+    [(False, 'g'), (False, 'h')],
+    [(False, 'f'), (False, 'i')],
+    [(False, 'f'), (False, 'h')],
+    [(False, 'f'), (False, 'g')],
+    [(False, 'e'), (False, 'i')],
+    [(False, 'e'), (False, 'h')],
+    [(False, 'e'), (False, 'g')],
+    [(False, 'e'), (False, 'f')],
+    [(False, 'd'), (False, 'i')],
+    [(False, 'd'), (False, 'h')],
+    [(False, 'd'), (False, 'g')],
+    [(False, 'd'), (False, 'f')],
+    [(False, 'd'), (False, 'e')],
+    [(False, 'c'), (False, 'i')],
+    [(False, 'c'), (False, 'h')],
+    [(False, 'c'), (False, 'g')],
+    [(False, 'c'), (False, 'f')],
+    [(False, 'c'), (False, 'e')],
+    [(False, 'c'), (False, 'd')],
+    [(False, 'b'), (False, 'i')],
+    [(False, 'b'), (False, 'h')],
+    [(False, 'b'), (False, 'g')],
+    [(False, 'b'), (False, 'f')],
+    [(False, 'b'), (False, 'e')],
+    [(False, 'b'), (False, 'd')],
+    [(False, 'b'), (False, 'c')],
+    [(False, 'a'), (False, 'i')],
+    [(False, 'a'), (False, 'h')],
+    [(False, 'a'), (False, 'g')],
+    [(False, 'a'), (False, 'f')],
+    [(False, 'a'), (False, 'e')],
+    [(False, 'a'), (False, 'd')],
+    [(False, 'a'), (False, 'c')],
+    [(False, 'a'), (False, 'b')],
+    [(True, 'a'), (True, 'b'), (True, 'c'), (True, 'd'), (True, 'e'), (True, 'f'), (True, 'g'), (True, 'h'), (True, 'i')]
+]
+
+
+def __unitResolutionForSet__(knf : list, units : set) -> list:
+    """makes unitresolution to given knf for list of clauses and for the newly created ones
 
     Args:
         knf list(list(tuple(bool, str))) : is a list of clauses
-        unitClause tuple(bool, str) : is the new unitclause
+        units set(tuple(bool, str)) : are the unitclauses
 
     Returns:
         list(list(tuple(bool, str))): is the new knf after resolution
     """
-
-    def makeUnitResolutionForASingleClause(knf : list, clause : tuple) -> tuple: #returns tuple(list, set) with list as new knf and set as new set of clauses
+    
+    def makeUnitResolution(knf : list, clauses : set) -> tuple: #returns tuple(list, set) with list as new knf and set as new set of unit-clauses
         tmpKnf = knf.copy()
         newUnitclauses = set()
         for cl in knf:
-            if clause in cl:
+            clSet = set(cl)
+            if len(clSet.intersection(clauses)) > 0:
                 tmpKnf.remove(cl)
-            elif (not clause[0], clause[1]) in cl:
-                tmpCl = cl.copy()
-                tmpCl.remove((not clause[0], clause[1]))
-                tmpKnf.remove(cl)
+                continue
+            
+            tmpCl = cl.copy()
+            appendNewClause = False
+            for clause in clauses:
+                if (not clause[0], clause[1]) in cl:
+                    tmpCl.remove((not clause[0], clause[1]))
+                    appendNewClause = True
+            
+            if appendNewClause:
+                tmpKnf.remove(cl)        
                 tmpKnf.append(tmpCl)
+                appendNewClause = False
+                
                 if len(tmpCl) == 1 and tmpCl[0] not in newUnitclauses:
                     newUnitclauses.update({(tmpCl[0])})
-
-        #add unitclause if neccesary
-        if [clause] not in tmpKnf:
-            tmpKnf.append([clause])
+                
+                
+        #add unitclauses if neccesary
+        for clause in clauses:
+            if [clause] not in tmpKnf:
+                tmpKnf.append([clause])
 
         return (tmpKnf, newUnitclauses)
 
-    newClauses = {unitClause}
+    newClauses = units
     while len(newClauses) > 0:
-        clause = newClauses.pop()
-        res = makeUnitResolutionForASingleClause(knf, clause)
-        newClauses.update(res[1])
+        res = makeUnitResolution(knf, newClauses)
+        newClauses = res[1]
         knf = res[0].copy()
 
     #remove duplicates
@@ -48,17 +103,17 @@ def unitResolution(knf : list, unitClause : tuple) -> list:
     for cl in knf:
         if cl not in tmpKnf:
             tmpKnf.append(cl.copy())
-
     return tmpKnf
 
+
 def solveSudoku(sudoku : list) -> list:
-    """solve sudoku with simple tree-like testing of possible states. Requires import of "sudokuGeneralKNF"
+    """solve sudoku with tree-like testing of possible states. Requires import of "sudokuGeneralKNF"
 
     Args:
         sudoku list(list(int)) : is a 2-dimensional array containing a 9 lists with 9 intergers, representing the field. If an int is 0, it is not set 
 
     Returns:
-        list(list(int)) : returns a solved sudoku game
+        list(list(int)) : returns a solved sudoku game or an empty list (if its not solveable)
     """
 
     def isInSolvedState(knf : list) -> bool:
@@ -124,50 +179,38 @@ def solveSudoku(sudoku : list) -> list:
 
         return tmpResult
 
-
     #read in the sudoku-game and construct the knf
     knf = sudokuGeneral.copy()
+    setOfUnitClauses = set()
     for row in range(0,9):
         for col in range(0,9):
             if sudoku[row][col] != 0:
-                knf = unitResolution(knf, (True, str(row) + str(col) + "-" + str(sudoku[row][col])))
-
-    #stack of lists, containing all assumptions, which still need to be tested
-
-    #contains still untried clauses and old knf
+                setOfUnitClauses.update({(True, str(row) + str(col) + "-" + str(sudoku[row][col]))})
+    knf = __unitResolutionForSet__(knf, setOfUnitClauses)
+                
+    #contains still untried clauses and old knf as tuples
     stackAddableUnitClauses = list()
 
     while not isInSolvedState(knf.copy()):
-
         if [] in knf and len(stackAddableUnitClauses) == 0:
-            raise Exception("sudoku was unsolveable")
+            return []
         elif [] in knf and len(stackAddableUnitClauses) > 0:
             #check other configuration -> you made a wrong assumption
-            print("conflict, try new guess")
 
-            #check for new assumption
             newClause = stackAddableUnitClauses.pop()
-            print(newClause[0][1].replace("-", " -> "))
-            knf = unitResolution(newClause[1], newClause[0])
+            knf = __unitResolutionForSet__(newClause[1], {newClause[0]})
         else:
             #test further config
-            print("not solved, make a further guess")
-
+            
             #get non-set field
             tmp = getUnallocatetField(knf)
-            row = tmp[0]
-            col = tmp[1]
-            allocations = tmp[2]
-
-            print(str(row) + str(col) + " -> " + str(allocations[0]))
-
             #make stack and try out allocations
-            for unit in allocations[1:]:
-                stackAddableUnitClauses.append(((True, str(row) + str(col) + "-" + str(unit)), knf.copy()))
-            knf = unitResolution(knf.copy(), (True, str(row) + str(col) + "-" + str(allocations[0])))
+            for unit in tmp[2][1:]:
+                stackAddableUnitClauses.append(((True, str(tmp[0]) + str(tmp[1]) + "-" + str(unit)), knf.copy()))
+            knf = __unitResolutionForSet__(knf, {(True, str(tmp[0]) + str(tmp[1]) + "-" + str(tmp[2][0]))})
 
 
-    #convert unit-clauses into list-structure
+    #convert unit-clauses into list-structure and return it
     result = [
         [0, 0, 0,   0, 0, 0,    0, 0, 0],
         [0, 0, 0,   0, 0, 0,    0, 0, 0],
@@ -184,7 +227,6 @@ def solveSudoku(sudoku : list) -> list:
     for clause in knf:
         if clause[0][0]:
             result[int(clause[0][1][0])][int(clause[0][1][1])] = int(clause[0][1][3])
-
     return result
 
 def printSudoku(sudoku : list) -> None:
@@ -196,7 +238,7 @@ def printSudoku(sudoku : list) -> None:
     Results:
         None
     """
-    tmp = "\n"
+    tmp = ""
     for row in range(0,9):
         tmpList = sudoku[row].__str__()
         tmpList = tmpList[0: 9] + "   " + tmpList[9: 18] + "   " + tmpList[18:]
@@ -205,20 +247,31 @@ def printSudoku(sudoku : list) -> None:
             tmp = tmp + "\n"
 
     print(tmp[:-2])
+    
+    
+    
+    
+    
+tmp = [
+    [0, 0, 5,   3, 0, 0,    0, 0, 0],
+    [8, 0, 0,   0, 0, 0,    0, 2, 0],
+    [0, 7, 0,   0, 1, 0,    5, 0, 0],
 
-result6 = [
-    [0, 0, 3,   9, 0, 0,    0, 0, 0],
-    [0, 0, 0,   1, 0, 7,    9, 0, 0],
-    [0, 0, 9,   0, 0, 5,    2, 0, 8],
+    [4, 0, 0,   0, 0, 5,    3, 0, 0],
+    [0, 1, 0,   0, 7, 0,    0, 0, 6],
+    [0, 0, 3,   2, 0, 0,    0, 8, 0],
 
-    [0, 0, 7,   0, 0, 0,    1, 0, 5],
-    [3, 0, 0,   0, 0, 0,    0, 0, 9],
-    [2, 0, 4,   0, 0, 0,    8, 0, 0],
-
-    [9, 0, 2,   5, 0, 0,    3, 0, 0],
-    [0, 0, 1,   7, 0, 6,    0, 0, 0],
-    [0, 0, 0,   0, 0, 9,    6, 0, 0]
+    [0, 6, 0,   5, 0, 0,    0, 0, 9],
+    [0, 0, 4,   0, 0, 0,    0, 3, 0],
+    [0, 0, 0,   0, 0, 9,    7, 0, 0]
 ]
+tmp = sudoku2
 
-result = solveSudoku(result6)
-printSudoku(result)
+startTime = time.time()
+print("It needs to solve:")
+printSudoku(tmp)
+print("\n\n")
+tmp = solveSudoku(tmp)
+print("Its solution:")
+printSudoku(tmp)
+print("time in seconds: " + str(time.time() - startTime))
